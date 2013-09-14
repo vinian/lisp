25f18c07fdf127dd32416a21e8e8e3ba99cf2e2c
VINIAN <willian.wnh@gmail.com>
2013-09-14 23:31:03 +0800

    pascal triangle

---
+++ pascal-triangle.lisp
@@ -0,0 +1,17 @@
+(defun f-pascal (x y)
+  'y must not big than x'
+  (if (or (= y 0) (= x y)) 1
+      (+ (f-pascal (- x 1) (- y 1)) (f-pascal (- x 1) y))))
+
+
+(defun print-pascal (x)
+  (loop for i from 0 to x do (format t "~a " (f-pascal x i))))
+
+
+(defun pascal (n)
+  (do ((index 0 (+ index 1)))
+      ((> index n))
+    (progn
+      (print-pascal index)
+      (format t "~%"))))
+
