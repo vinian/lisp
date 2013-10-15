e043fc8326a756a4a009d83dff4747d0a29b88a6
VINIAN <willian.wnh@gmail.com>
2013-10-16 01:16:28 +0800

    split add max count

--- split-string.lisp
+++ split-string.lisp
@@ -1,6 +1,6 @@
 (defun split-string-by-sep (sep str &optional count)
   "split str by sep"
-  (let ((pos-str (get-sep-pos-in-str sep str))
+  (let ((pos-str (get-sep-pos-in-str sep str count))
         (last-pos 0)
         (result))
     (dolist (pos pos-str)
@@ -10,9 +10,13 @@
     (nreverse result)))
 
 
-(defun get-sep-pos-in-str (sep str)
+(defun get-sep-pos-in-str (sep str &optional count)
   "find the position of all apperance of char in str"
   (do ((result)
-       (pos (position sep str) (position sep str :start (1+ pos))))
-      ((null pos) (nreverse result))
+       (pos (position sep str) (position sep str :start (1+ pos)))
+       (inner-count 1 (1+ inner-count)))
+      ((or
+        (null pos)
+        (and (not (null count)) (= inner-count count)))
+       (nreverse result))
     (push pos result)))
